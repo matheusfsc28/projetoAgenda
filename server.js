3456ce23ec007fe0819d1f46345c3762c2d0ad0d
matheusfsc28 <matheus.felipescruz@gmail.com>
2023-09-15 14:50:52 -0300

    Corrigido problemas de caminho

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         console.log('Conectado');
         app.emit('pronto');
