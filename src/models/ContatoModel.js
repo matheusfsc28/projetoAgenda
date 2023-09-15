80930f469c37cbbc6da5a198876c6cd4952e42eb
matheusfsc28 <matheus.felipescruz@gmail.com>
2023-09-15 14:47:45 -0300

    Adicionado função de deletar contato

--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -17,11 +17,6 @@ function Contato(body) {
     this.contato = null;
 }
 
-Contato.buscaPorId = async function(id){
-    if(typeof id !== 'string') return
-    const user = await ContatoModel.findById(id)
-    return user
-}
 
 Contato.prototype.register = async function () {
     this.valida();
@@ -59,7 +54,27 @@ Contato.prototype.edit = async function (id) {
     this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, {new: true})
 }
 
+// Métodos estataticos
+Contato.buscaPorId = async function(id){
+    if(typeof id !== 'string') return
+    const contato = await ContatoModel.findById(id)
+    return contato
+}
 
+
+Contato.buscaContatos = async function(){
+    const contatos = await ContatoModel.find()
+        .sort({criadoEm: -1})
+    return contatos
+}
+
+Contato.delete = async function(id){
+    if(typeof id !== 'string') return
+
+    const contato = await ContatoModel.findOneAndDelete({_id: id})
+
+    return contato
+}
 
 const ContatoModel = mongoose.model('Contato', ContatoSchema);
 
